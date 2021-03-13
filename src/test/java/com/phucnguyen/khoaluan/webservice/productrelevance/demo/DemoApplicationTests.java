af784db403e0ccf84c45d9b99cbcc53dcd2865f7
Phuc Nguyen <nhp200199@gmail.com>
2021-03-13 15:26:42 +0700

    first commit

---
+++ src/test/java/com/phucnguyen/khoaluan/webservice/productrelevance/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucnguyen.khoaluan.webservice.productrelevance.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
