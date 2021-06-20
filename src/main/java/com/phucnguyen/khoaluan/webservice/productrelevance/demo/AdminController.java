afcd54dd0a0828bfed9a94c99182344b5d8b62b5
Phuc Nguyen <nhp200199@gmail.com>
2021-06-20 15:42:35 +0700

    add CORS

--- src/main/java/com/phucnguyen/khoaluan/webservice/productrelevance/demo/AdminController.java
+++ src/main/java/com/phucnguyen/khoaluan/webservice/productrelevance/demo/AdminController.java
@@ -4,14 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 
+@CrossOrigin(origins = "*", 
+    methods = {RequestMethod.POST, RequestMethod.DELETE, RequestMethod.GET}
+)
 @RestController
 public class AdminController {
     @Autowired
